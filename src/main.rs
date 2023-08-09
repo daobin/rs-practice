f127279db6b53e2840410366ff4b92651229ae52
daobin <shouwenlai@foxmail.com>
2023-08-09 13:40:25 +0800

    初始化 cargo 项目

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    let age = 23;
+    println!("Age: {}", age);
+    let mut age = "33";
+    println!("Age: {}", age);
+    age = "中年啦";
+    println!("Age: {}", age);
+}
